ac0f5ca30442d5228fb36fe468a29405129db0f7
lfitori <luisfitoria91@gmail.com>
2019-09-06 20:17:59 -0600

    addcors

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const { router } = require("./routes/routes");
 const MONGOURI = require("./config/keys").mongoURI;
 const PORT = process.env.PORT || 4200;
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
 //Config body express app
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors);
 
 mongoose.connect(MONGOURI, {useNewUrlParser: true})
     .then(() => console.log("Conectado a la base de datos 🚀"))
